cced1190caa345034043ad432c41848880f5afc8
111202113215@mhs.dinus.ac.id <111202113215@mhs.dinus.ac.id>
2024-09-25 15:49:29 +0700

    t

--- config/app.php
+++ config/app.php
@@ -160,6 +160,7 @@ return [
          * Package Service Providers...
          */
         App\Providers\FortifyServiceProvider::class,
+        Tymon\JWTAuth\Providers\LaravelServiceProvider::class,
         /*
          * Application Service Providers...
          */
